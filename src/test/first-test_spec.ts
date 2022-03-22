b92529fd3e6c8472f8b248be5df117b5c38c5b3f
yuto-yuto <yuto.nakatsu@gmail.com>
2022-03-22 20:43:26 +0100

    Update first-test code

--- src/test/first-test_spec.ts
+++ src/test/first-test_spec.ts
@@ -1,32 +1,45 @@
 import "mocha";
 import { expect } from "chai";
-import { RankingHolder } from "../first-test";
+import { RankingHolder } from "../lib/first-test";
 
 describe("RankingHolder", () => {
-    describe("Average", () => {
-        it("should return null if no result is added", () => {
-            const instance = new RankingHolder();
-            expect(instance.average).to.be.null;
+    describe("average", () => {
+        let instance: RankingHolder;
+
+        beforeEach(() => {
+            instance = new RankingHolder();
         });
 
-        it("should return 50 if a result with score 50 is added", () => {
-            const instance = new RankingHolder();
-            instance.add({ name: "Yuto", score: 50 });
-            expect(instance.average).to.equal(50);
+        context('when having no result', () => {
+            it("should return null", () => {
+                expect(instance.average).to.be.null;
+            });
         });
 
-        it("should return 55 when adding 40, 50, 60 and 70", () => {
-            const instance = new RankingHolder();
-            instance.add({ name: "Yuto", score: 40 });
-            instance.add({ name: "Yuto2", score: 50 });
-            instance.add({ name: "Yuto3", score: 60 });
-            instance.add({ name: "Yuto4", score: 70 });
-            expect(instance.average).to.equal(55);
+        context('when having multiple results', () => {
+            it("should return 50 if a result with score 50 is added", () => {
+                instance.add({ name: "Yuto", score: 50 });
+                expect(instance.average).to.equal(50);
+            });
+
+            it("should return 55 when adding 40, 50, 60 and 70", () => {
+                instance.add({ name: "Yuto", score: 40 });
+                instance.add({ name: "Yuto2", score: 50 });
+                instance.add({ name: "Yuto3", score: 60 });
+                instance.add({ name: "Yuto4", score: 70 });
+                expect(instance.average).to.equal(55);
+            });
+
+            it("should return -2 when adding -6, and 2", () => {
+                instance.add({ name: "Yuto", score: -6 });
+                instance.add({ name: "Yuto2", score: 2 });
+                expect(instance.average).to.equal(-2);
+            });
         });
     });
 
-    describe("HighestScore", () => {
-        it("should return null if no result is added", () => { 
+    describe("highestScore", () => {
+        it("should return null if no result is added", () => {
             const instance = new RankingHolder();
             expect(instance.highestScore).to.be.null;
         });
