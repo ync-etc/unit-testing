640fee381b65aece2241c02b443ed894a632d64f
yuto-yuto <yuto.nakatsu@gmail.com>
2022-03-26 09:40:49 +0100

    Change res path

--- src/lib/typing-game/SentenceGenerator.ts
+++ src/lib/typing-game/SentenceGenerator.ts
@@ -6,13 +6,13 @@ export class SentenceGenerator {
 
     public async load(): Promise<void> {
         // This way doesn't allow us to use a test file in unit test
-        const resourceDir = path.join(__dirname, "../res");
+        const resourceDir = path.join(__dirname, "../../res");
         // This function can be stubbed
         this.sentences = await loadSentenceFiles(resourceDir);
     }
 
     public async load2(): Promise<void> {
-        const resourceDir = process.env.CONFIG_DIR || path.join(__dirname, "../res");
+        const resourceDir = process.env.CONFIG_DIR || path.join(__dirname, "../../res");
         this.sentences = await loadSentenceFiles(resourceDir);
     }
 
